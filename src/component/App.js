f0ff764e78954164da01f186913ac35e74b31952
Murat <mrtgms3561@gmail.com>
2021-03-14 19:54:25 +0200

    First step

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchBar from './SearchBar'
+
+const App = () => {
+    return(
+        <div>
+            <h1>React Pic Search</h1>
+            <SearchBar/>
+        </div>
+    )
+}
+
+export default App
